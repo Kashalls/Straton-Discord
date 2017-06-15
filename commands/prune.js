b8dc13ded2bbca1e769f5d606f20970fd0582479
Kashall <Kashalls@users.noreply.github.com>
2017-06-14 20:53:46 -0700

    Updates

--- commands/prune.js
+++ commands/prune.js
@@ -4,21 +4,24 @@ exports.run = (client, message, args) => {
     if (message.member.hasPermission("MANAGE_MESSAGES")) {
         if (message.guild.member(client.user).hasPermission("MANAGE_MESSAGES")) {
 
-            if (args.length == 0 ) {
-                return message.reply("You didn't seem to have put an ammount. Lets try this again... EX: /prune <2-100>"); 
+            if (!args) {
+                return message.channel.send(`:grey_question: Please provide a value: /prune <2-100>`);
             } else {
-                try {
-                    message.channel.bulkDelete(args[0]).then(messages => {
-                        return message.channel.send(`${args} messages were cleared out.`).then((sent) => {
-                            sent.delete(15000);
+                let value = isNaN(args[0]);
+                if (value == false) {
+                    if (args[0] >= 5 && args[0] <= 100) {
+                        message.channel.bulkDelete(args[0]).then(messages => {
+                        return message.channel.send(`:checkered_flag: ${args} messages were cleared out.`);
                         })
-                    })
-                } catch (err) {
-                    console.error(err);
+                    } else {
+                        return message.channel.send(`:warning: Your value has to be between 5 and 100.`);
+                    }
+                } else {
+                    return message.channel.send(`:warning: You provided: ${args[0]}, but it has to be a number between 5 and 100!`).then((sent) => { sent.delete(15000); });
                 }
             }
         } else {
-            return message.channel.send(`Sorry, I don't have permission to {MANAGE_MESSAGES} in this guild!`);
+            return message.channel.send(`I don't have permission to {MANAGE_MESSAGES} in this guild!`);
         }
     }
 }
---
+++ events/message.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args) => {
+
+    //message.content shows the content of the message
+    if (message.member.roles.size > 1 ) {
+        return;
+    } else {
+        let sentMessage = message.content;
+        const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+        var found = sentMessage.match(regex);
+
+        if (!found) {
+            return;
+        } else {
+            message.delete();
+            client.channels.get('319302806744072194').send(`:x: ${member.user.username} has no rank and tried to post a link!`);
+            return message.author.send(':x: Sorry about that ' +  message.author.username + `, but we don't allow posting links in our Discord unless you have a rank.`);
+        }
+    }
+}
